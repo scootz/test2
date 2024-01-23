289b903934d3676004899efa0fad6fb59ab35cb9
Scott Marshall <marshals@gmail.com>
2024-01-22 23:10:32 -0500

    fixed null errors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,8 @@ import App from "./App.vue";
 createApp(App).mount("#app");
 
 import { appWindow } from '@tauri-apps/api/window'
-document
-  .getElementById('titlebar-minimize')
-  .addEventListener('click', () => appWindow.minimize())
-document
-  .getElementById('titlebar-maximize')
-  .addEventListener('click', () => appWindow.toggleMaximize())
-document
-  .getElementById('titlebar-close')
-  .addEventListener('click', () => appWindow.close())
+window.document.getElementById('titlebar-minimize')?.addEventListener('click', () => appWindow.minimize())
+window.document.getElementById('titlebar-maximize')?.addEventListener('click', () => appWindow.toggleMaximize())
+window.document.getElementById('titlebar-close')?.addEventListener('click', () => appWindow.close())
+
+  
